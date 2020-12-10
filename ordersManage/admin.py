3d199f81502a7d2ed6f980bbcf3501c82f5b0357
Antonio Hellin <antonio.hellinh@gmail.com>
2020-12-10 08:09:20 +0100

    Using Forms and get information from database

--- ordersManage/admin.py
+++ ordersManage/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from ordersManage.models import Client, Article, Order
 
-class ClientsAdmin(admin.ModelAdmin):
-    
+class ClientsAdmin(admin.ModelAdmin): 
     list_display = ("name", "address", "email", "phone")
     search_fields = ("name", "email")
 
+class ArticlesAdmin(admin.ModelAdmin):
+    list_filter = ("name", "section")
+    search_fields = ("name", "section")
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ("number", "date")
+    list_filter = ("date", )
+    date_hierarchy = "date"
+
+
 admin.site.register(Client, ClientsAdmin)
-admin.site.register(Article)
-admin.site.register(Order)
+admin.site.register(Article, ArticlesAdmin)
+admin.site.register(Order, OrdersAdmin)
--- ordersManage/views.py
+++ ordersManage/views.py
@@ -1,3 +1,25 @@
+from ordersManage.models import Article
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def search_products(request):
+
+    return render(request, "search_products.html")
+
+def search(request):
+
+    if request.GET["prd"]:
+        #message = "Article found: %r" %request.GET["prd"]
+
+        product = request.GET["prd"]
+
+        articles = Article.objects.filter(name__icontains=product)
+
+        return render(request, "result_search.html", {"articles":articles, "query":product})
+    
+    else:
+        message="You didnt insert anything."
+
+    return HttpResponse(message)
