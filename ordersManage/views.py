cd1c4d21c99e2d14d77c826556f3429fa3da126d
Antonio Hellin <antonio.hellinh@gmail.com>
2020-12-11 13:32:01 +0100

    Ussing API Forms with Django

--- ordersManage/views.py
+++ ordersManage/views.py
@@ -1,8 +1,10 @@
 from ordersManage.models import Article
+from ordersManage.forms import ContactForm
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+
 
 # Create your views here.
 
@@ -29,19 +31,41 @@ def search(request):
 
     return HttpResponse(message)
 
+# def contact(request):
+
+#    if request.method == "POST":
+
+#        subject = request.POST["subject"]
+#        message = request.POST["messageContact"] + " " + request.POST["email"]
+#        email_from = settings.EMAIL_HOST_USER
+
+#       recipient_list = ["antonio.hh7@gmail.com"]
+
+#        send_mail(subject, message, email_from, recipient_list)
+
+
+#        return render(request, "thanks.html")
+
+#    return render(request, "contact.html")
+
+#Using API FORMS
+
 def contact(request):
 
     if request.method == "POST":
 
-        subject = request.POST["subject"]
-        message = request.POST["messageContact"] + " " + request.POST["email"]
-        email_from = settings.EMAIL_HOST_USER
+        myForm = ContactForm(request.POST)
 
-        recipient_list = ["antonio.hh7@gmail.com"]
+        if myForm.is_valid():
 
-        send_mail(subject, message, email_from, recipient_list)
+            cleanFormData = myForm.cleaned_data
 
+            send_mail(cleanFormData['subject'], cleanFormData['email'], cleanFormData.get('email',''), ['antonio.hh7@gmail.com'],)
 
-        return render(request, "thanks.html")
+            return render(request, "thanks.html")
+    
+    else:
 
-    return render(request, "contact.html")
+        myForm = ContactForm()
+    
+    return render(request, "contact_form.html", {"form" : myForm})
