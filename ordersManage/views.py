a1cd7442a3deb19ac8b1f02b686bbbbad048fb23
Antonio Hellin <antonio.hellinh@gmail.com>
2020-12-11 10:25:15 +0100

    Using the library core.mail and the function send_mail()

--- ordersManage/views.py
+++ ordersManage/views.py
@@ -1,6 +1,8 @@
 from ordersManage.models import Article
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -15,11 +17,31 @@ def search(request):
 
         product = request.GET["prd"]
 
-        articles = Article.objects.filter(name__icontains=product)
+        if len(product)>20:
+            message = "Article is too long."
 
-        return render(request, "result_search.html", {"articles":articles, "query":product})
+        else:
+            articles = Article.objects.filter(name__icontains=product)
+            return render(request, "result_search.html", {"articles":articles, "query":product})
     
     else:
         message="You didnt insert anything."
 
     return HttpResponse(message)
+
+def contact(request):
+
+    if request.method == "POST":
+
+        subject = request.POST["subject"]
+        message = request.POST["messageContact"] + " " + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+
+        recipient_list = ["antonio.hh7@gmail.com"]
+
+        send_mail(subject, message, email_from, recipient_list)
+
+
+        return render(request, "thanks.html")
+
+    return render(request, "contact.html")
